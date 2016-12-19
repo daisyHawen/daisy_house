068802c67283a52d4c490faf8964daa4d1fa0f0d
daisyHawen <512309453@qq.com>
2016-12-19 17:14:51 +0800

    all

---
+++ src/components/News.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+	<ul>
+		<li v-for="news in newsList">
+            <router-link :to="{ name: 'detail', params: { id: news.id}, query: { info: news.query } }">{{news.title}}</router-link>
+		</li>
+	</ul>
+	<div>
+		<router-view></router-view>
+	</div>
+</div>
+</template>
+
+<script>	
+	export default{
+		data: function(){
+			return {
+				newsList: [
+					{ id: '01', title: 'News 01',query:'xyx'},
+					{ id: '02', title: 'News 02'},
+					{ id: '03', title: 'News 03'}
+				]
+			}
+		}
+	}	
+</script>
